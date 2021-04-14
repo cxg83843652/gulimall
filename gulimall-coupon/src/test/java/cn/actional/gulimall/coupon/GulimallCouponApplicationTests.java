24bb560530b60538f16af9f84a23a18be2b9a5ba
Actional <854356662@qq.com>
2021-04-14 23:02:38 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/cn/actional/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.actional.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-product/src/main/java/cn/actional/gulimall/product/GulimallProductApplication.java
@@ -0,0 +1,13 @@
+package cn.actional.gulimall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProductApplication.class, args);
+    }
+
+}
